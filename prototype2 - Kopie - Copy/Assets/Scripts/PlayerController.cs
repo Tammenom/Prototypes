ee261d3707d56a0ca08f2169869c82d2b3bb98a5
kuehnleinlukas <lukas.kuehnlein@posteo.de>
2017-12-09 23:18:09 +0100

    upload prototype211

---
+++ prototype2 - Kopie - Copy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float moveSpeed;
+    public float upDownSpeed;
+
+    public bool canMove;
+
+    private Animator anim;
+    private Rigidbody2D playerRigidBody;
+
+    private bool PlayerMovingHorizontal;
+    private bool PlayerMovingVertical;
+    private float LastMove;
+
+    private static bool playerExists = false;
+
+
+    // Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        canMove = true;
+
+        if (!playerExists)
+        {
+            
+            DontDestroyOnLoad(transform.gameObject);
+            playerExists = true;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        PlayerMovingHorizontal = false;
+        PlayerMovingVertical = false;
+
+        if (!canMove) {
+            playerRigidBody.velocity = Vector2.zero;
+            return;
+        }
+
+
+        if (Input.GetAxisRaw("Horizontal")> 0.5f || Input.GetAxisRaw("Horizontal") < -0.5)
+        {
+            //transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, playerRigidBody.velocity.y);
+            PlayerMovingHorizontal = true;
+            LastMove = Input.GetAxisRaw("Horizontal");
+        }
+
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5)
+        {
+            //transform.Translate(new Vector3 (0f, Input.GetAxisRaw("Vertical") * upDownSpeed * Time.deltaTime, 0f));
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * upDownSpeed);
+            PlayerMovingVertical = true;
+        }
+
+        if(Input.GetAxisRaw("Horizontal") <0.5f && Input.GetAxisRaw("Horizontal") > -0.5)
+        {
+            playerRigidBody.velocity = new Vector2(0f, playerRigidBody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5)
+        {
+            playerRigidBody.velocity = new Vector2( playerRigidBody.velocity.x, 0f);
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerHorizontalMoving", PlayerMovingHorizontal);
+        anim.SetBool("PlayerVerticalMoving", PlayerMovingVertical);
+        anim.SetFloat("LastMoveX", LastMove);
+
+    
+    }
+}
---
+++ prototype2 - Kopie - Copy/Assets/Scripts/enter_Inside.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enter_Inside : MonoBehaviour{
+
+    
+    public GameObject inBox;
+    public GameObject cabine;
+    public GameObject outerCabine;
+    public string hello;
+    public Camera incam;
+    public CameraController dd;
+
+    // Use this for initialization
+    void Start()
+    {
+        inBox.SetActive(false);
+        cabine.SetActive(false);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dd = FindObjectOfType<CameraController>();
+
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            inBox.SetActive(true);
+            cabine.SetActive(true);
+            dd.SetCamMin();
+            outerCabine.SetActive(false);
+
+
+
+
+
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            inBox.SetActive(false);
+            cabine.SetActive(false);
+            dd.SetCamMax();
+            outerCabine.SetActive(true);
+
+        }
+
+    }
+}
